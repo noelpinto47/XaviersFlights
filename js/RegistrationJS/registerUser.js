0b268380d0bbd1cb7eec50db912e685258a6cc34
Noel Pinto <noelpinto47@gmail.com>
2023-04-03 15:46:01 +0530

    updated

--- js/RegistrationJS/registerUser.js
+++ js/RegistrationJS/registerUser.js
@@ -64,11 +64,31 @@ document.querySelector('#button-1').addEventListener('click', function(){
             }
             
         }
+      
+      
+        const validateEmail = (email) => {
+          return String(email)
+            .toLowerCase()
+            .match(
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+        };
 
+        var emailValid = false;
+        if(validateEmail(email)){
+          console.log("email valid");
+          emailValid = true;
+        }
+        else{
+          console.log("invalid email");
+        }
 
-  if(password.localeCompare(confirm_password) != 0)
-  {
-    alert("Please enter matching password");
+  if(password.localeCompare(confirm_password) != 0 || emailValid == false)
+  { 
+    if(password.localeCompare(confirm_password) != 0)
+      alert("Please enter matching password");
+    if(emailValid == false)
+      alert("Please enter a valid email address");
   }
   else{
       if (confirm("Register User?")) {
